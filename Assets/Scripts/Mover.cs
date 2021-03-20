a76cae318136fb0baddc9cf72f27cc236e15fbee
Felipe Camêlo <felipecamelobrx@gmail.com>
2021-03-20 19:47:30 -0300

    completed the basics from lecture

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,20 +6,24 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float jumpForce = 10f;
 
     //STRING REFERENCES
     static string HORIZONTAL = "Horizontal";
     static string VERTICAL = "Vertical";
+    static string JUMP = "Jump";
 
+    Rigidbody rigidBody;
 
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         Move();
+        Jump();
     }
 
     private void Move()
@@ -27,5 +31,13 @@ public class Mover : MonoBehaviour
         float xMovement = Input.GetAxis(HORIZONTAL) * Time.deltaTime * moveSpeed;
         float zMovement = Input.GetAxis(VERTICAL) * Time.deltaTime * moveSpeed;
         transform.Translate(xMovement, 0, zMovement);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidBody.velocity = new Vector3(0, jumpForce, 0);
+        }
     }
 }
